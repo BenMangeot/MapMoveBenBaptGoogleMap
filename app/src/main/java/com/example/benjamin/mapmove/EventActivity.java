970ff90b4034c022f25b64f00980a433ef62ef44
Baptiste Lestavel <baptiste.lestavel@hei.fr>
2017-04-26 17:18:23 +0200

    Activity Detail Event

--- app/src/main/java/com/example/benjamin/mapmove/EventActivity.java
+++ app/src/main/java/com/example/benjamin/mapmove/EventActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toolbar;
